39ed431479d9b839ec35814095f17dcf7d97576b
Username <@DESKTOP-9JU9P37.lan>
2018-12-09 11:02:27 +0800

    add

--- src/main/java/studyGig/HelloWorld.java
+++ src/main/java/studyGig/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		int a=1;
+		System.out.println(a);
 	}
 
 }
