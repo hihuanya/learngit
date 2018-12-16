3bfd8461d5902a5408340dde7e307eae8340d9bd
Username <@DESKTOP-9JU9P37.lan>
2018-12-16 17:54:55 +0800

    111

--- src/main/java/studyGig/HelloWorld.java
+++ src/main/java/studyGig/HelloWorld.java
@@ -1,15 +1,21 @@
 package studyGig;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
-		int a=1;
-		System.out.println(a);
-		System.out.println(a);
-		System.out.println(a);
-		System.out.println(a);
+		System.out.println("study in gitHub");
+		System.out.println("³åÍ»2222");
+		System.out.println("study in gitHub");
+		System.out.println("³åÍ»");
 	}
 
 }
---
+++ src/main/java/studyGig111/HellSayHello.java
@@ -0,0 +1,10 @@
+package studyGig111;
+
+public class HellSayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÃÌº”–≈œ¢");
+	}
+
+}
